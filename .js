da65e0bc1ecd81b1f6a3cd42ac010fc62274d68d
Jacob Dowdy <jacobdowdy@rocketmail.com>
2017-06-27 21:21:54 -0400

    Update .js

--- .js
+++ .js
@@ -2,7 +2,7 @@
 function chunkArrayInGroups(arr, size) {
 
   var newArr = [];
-  for (var i = 1; i < arr.length; i+=size) {
+  for (var i = 0; i < arr.length; i+=size) {
     newArr.push(arr.slice(i, i+size));
   }
   return newArr;
